fc4d8facf0892da1dd2e4dde624062044cd0090a
Art Karasev <art@swordfox.nz>
2023-04-18 11:12:59 +1200

    initial commit

--- _config.php
+++ _config.php
@@ -1,4 +1,5 @@
 <?php
 
-// You need this file if you don't have anything in the _config folder. If that folder exists
-// and is not empty then you can delete this file.
+use Swordfox\Vite\Helpers\Vite;
+
+Vite::adminAssets();
