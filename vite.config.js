d8540d32b2fd46a0e18291517f4e845658915b0d
Art Karasev <art@swordfox.nz>
2023-04-18 13:42:05 +1200

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,38 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+// import { viteStaticCopy } from 'vite-plugin-static-copy'
+// import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
     plugins: [
+
+        // viteStaticCopy({
+        //   targets: [
+        //     {
+        //       src: 'themes/custom/src/images/*',
+        //       dest: 'public/images',
+        //     },
+        //     // {
+        //     //   src: path.resolve(__dirname, 'themes/custom/src/fonts/*'),
+        //     //   dest: './public/fonts'
+        //     // }
+        //   ]
+        // }),
+
+        // copy({
+        //   targets: [{ src: 'themes/custom/src/images/*', dest: 'public/build/images' }]
+        // }),
+
         laravel({
             input: [
                 'themes/custom/src/app.scss',
                 'themes/custom/src/app.js',
-                // 'themes/custom/cms/cms.scss',
-                // 'themes/custom/cms/wysiwyg.scss',
+                'themes/custom/cms/cms.scss',
+                'themes/custom/cms/wysiwyg.scss',
             ],
             refresh: { paths: [
                 'themes/custom/templates/**',
             ]}
-        }),
-        viteStaticCopy({
-          targets: [
-            {
-              src: 'themes/custom/src/images',
-              dest: 'public/images'
-            },
-            {
-              src: 'themes/custom/src/fonts',
-              dest: 'public/fonts'
-            }
-          ]
         })
     ]
 });
